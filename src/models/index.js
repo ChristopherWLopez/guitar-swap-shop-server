69b5cc0fc4a27d1dd69dbaf72125cf234853c9b8
christopher lopez <christopherwlopez89@gmail.com>
2023-10-31 19:35:37 -0700

    created my index for my source which sets up my instances of sequalize

---
+++ src/models/index.js
@@ -0,0 +1,28 @@
+const { Sequelize } = require('sequelize');
+const { makeUser } = require('./userModel');
+
+
+const DATABASE_URL = 
+    process.env.NODE_ENV === 'test'
+        ? 'sqlite::memory:'
+        :process.env.DATABASE_URL;
+
+const CONNECTION_OPTIONS = 
+    process.env.NODE_ENV === 'test'
+        ? {
+            logging: false,
+        }
+        :{
+            ssl:{
+                require: true,
+                rejectUnauthorized: false,
+            },
+        };
+const sequelize = new Sequelize(DATABASE_URL, CONNECTION_OPTIONS);
+
+const User = makeUser(sequelize);
+
+module.exports = {
+    sequelize,
+    User,
+};
